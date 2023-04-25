559dcbb29c2d8b27d70648ae2282c6d7156d0f51
Petr Škoda <skodapetr@gmail.com>
2023-04-25 22:35:24 +0200

    Add support for dark mode

--- client/application/application.tsx
+++ client/application/application.tsx
@@ -1,9 +1,14 @@
+import {useMemo} from "react";
 import Container from "@mui/material/Container";
-import i18n, { InitOptions } from "i18next";
-import { initReactI18next } from "react-i18next";
+import CssBaseline from '@mui/material/CssBaseline';
+import useMediaQuery from "@mui/material/useMediaQuery";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import i18n, {InitOptions} from "i18next";
+import {initReactI18next} from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-import { HomeView } from "../home-view";
+
+import {HomeView} from "../home-view";
 
 // https://react.i18next.com/latest/using-with-hooks
 i18n
@@ -17,15 +22,29 @@ i18n
       escapeValue: false,
     },
     backend: {
-      // for all available options read the backend's repository readme file
-      loadPath: './locales/{{lng}}/{{ns}}.json'
+      // for all available options read the backend"s repository readme file
+      loadPath: "./locales/{{lng}}/{{ns}}.json"
     }
   } as InitOptions);
 
 export function Application() {
+  // Automatic dark mode support.
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = useMemo(() =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <Container fixed>
-      <HomeView />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Container fixed>
+        <HomeView/>
+      </Container>
+    </ThemeProvider>
   );
 }
