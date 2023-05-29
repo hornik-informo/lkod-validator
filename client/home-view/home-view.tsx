86cae6d4323c75f838bc8436ab81b4333e9e4499
Petr Škoda <skodapetr@gmail.com>
2023-05-29 21:49:30 +0200

    Replace view type with details

--- client/home-view/home-view.tsx
+++ client/home-view/home-view.tsx
@@ -1,27 +1,28 @@
-import React, { useState } from "react";
-import { useTranslation } from "react-i18next";
+import React, {useState, useMemo} from "react";
+import {useTranslation} from "react-i18next";
 import Switch from "@mui/material/Switch";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Box from "@mui/material/Box";
+import Button from '@mui/material/Button';
 
-import { useValidatorService } from "./validator-service";
-import { InputArea } from "./input-area";
-import { StatusBar } from "./status-bar";
-import { MessageList } from "./message-list";
-import { useMessageService } from "./message-service";
-import { useSummaryService } from "./summary-service";
-import { Summary } from "./summary";
+
+import {useValidatorService} from "./validator-service";
+import {InputArea} from "./input-area";
+import {StatusBar} from "./status-bar";
+import {MessageList} from "./message-list";
+import {useMessageService} from "./message-service";
+import {useSummaryService} from "./summary-service";
+import {Summary} from "./summary";
 
 export function HomeView() {
-  const { t } = useTranslation();
+  const {t} = useTranslation();
   const messageService = useMessageService();
   const summaryService = useSummaryService();
-  const { state, onBeginValidation } = useValidatorService([
+  const {state, onBeginValidation} = useValidatorService([
     messageService.listener,
     summaryService.listener,
   ]);
-  const [simpleMode, setSimpleMode] = useState(true);
   return (
     <>
       <h1>{t("home-view.title")}</h1>
@@ -34,9 +35,7 @@ export function HomeView() {
         message={state.statusMessage}
         args={state.statusArgs}
       />
-      {renderSwitch(t, simpleMode, setSimpleMode)}
       {renderContent(
-        simpleMode,
         state.working || state.completed,
         summaryService,
         messageService
@@ -45,40 +44,46 @@ export function HomeView() {
   );
 }
 
-function renderSwitch(t, value, onChange) {
-  return (
-    <FormGroup>
-      <FormControlLabel
-        control={<Switch checked={value} onChange={() => onChange(!value)} />}
-        label={t("home-view.show-simplified")}
-      />
-    </FormGroup>
-  );
-}
-
 function renderContent(
-  simpleView: boolean,
   ready: boolean,
   summaryService,
   messageService
 ) {
+  const {t} = useTranslation();
+  const [showDetails, setShowDetails] = useState(false);
   if (!ready) {
     return (
-      <Box display="flex" sx={{ justifyContent: "center" }}>
-        <br />
+      <Box display="flex" sx={{justifyContent: "center"}}>
+        <br/>
         Výsledky se zde zobrazí po spuštění validace.
       </Box>
     );
   }
-  if (simpleView) {
+
+  if (showDetails) {
     return (
-      <Summary
-        catalog={summaryService.state.catalog}
-        datasets={summaryService.state.datasets}
-        entrypoint={summaryService.state.entrypoint}
-      />
+      <>
+        <Summary
+          catalog={summaryService.state.catalog}
+          datasets={summaryService.state.datasets}
+          entrypoint={summaryService.state.entrypoint}
+        />
+        <MessageList groups={messageService.state.groups}/>
+      </>
     );
   } else {
-    return <MessageList groups={messageService.state.groups} />;
+    return (
+      <>
+        <Summary
+          catalog={summaryService.state.catalog}
+          datasets={summaryService.state.datasets}
+          entrypoint={summaryService.state.entrypoint}
+        />
+        <br/>
+        <Button variant="outlined" onClick={() => setShowDetails(true)}>
+          {t("home-view.show-details")}
+        </Button>
+      </>
+    );
   }
 }
