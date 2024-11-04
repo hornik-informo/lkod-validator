b4355a36089385918fe8e0717d037960ca11b3a6
Petr Škoda <skodapetr@gmail.com>
2024-11-04 11:20:02 +0100

    Refactoring

--- test/server.py
+++ test/server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from http.server import SimpleHTTPRequestHandler
 import socketserver
 
 class CORSRequestHandler (SimpleHTTPRequestHandler):
